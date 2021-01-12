9292e97071c4941d686e9d7044c78e30d3cfb571
Zugemibo <41533197+Zugemibo@users.noreply.github.com>
2021-01-12 18:18:08 +0100

    Added contact form for future purposes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';  
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ContactComponent } from './contact/contact.component';
 import { ServicesComponent } from './services/services.component';
+import { ContactService } from './contact.service';
+
 
 @NgModule({
   declarations: [
@@ -17,9 +21,13 @@ import { ServicesComponent } from './services/services.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
 
   ],
-  providers: [],
+  providers: [
+    ContactService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, NgModule } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,28 @@ import { Component, OnInit, NgModule } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  FormData: FormGroup;
+
+  constructor(private builder: FormBuilder, private contact: ContactService) { }
 
   ngOnInit(): void {
+    this.FormData = this.builder.group({
+      Fullname: new FormControl('', [Validators.required]),
+      Phone: new FormControl('', [Validators.required], ),
+      Email: new FormControl('', [Validators.compose([Validators.required, Validators.email])]),
+      Comment: new FormControl('', [Validators.required])
+      })
   }
+  onSubmit(FormData) {
+    console.log(FormData)
+    this.contact.PostMessage(FormData)
+    .subscribe(response => {
+    location.href = 'TEST'
+    console.log(response)
+    }, error => {
+    console.warn(error.responseText)
+    console.log({ error })
+    })
 
+}
 }
